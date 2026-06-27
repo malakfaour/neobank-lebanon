6ffedf02bd85cf8d661d3b336b5c6df580520896
noura <nourashalhoub2@gmail.com>
2026-06-27 14:43:38 +0300

    DEVATTECH-20 define beneficiaries schema

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,9 @@
+from app.models.beneficiary import Beneficiary, BeneficiaryType
+from app.models.notification import Notification, NotificationType
+
+__all__ = [
+    "Notification",
+    "NotificationType",
+    "Beneficiary",
+    "BeneficiaryType",
+] 
